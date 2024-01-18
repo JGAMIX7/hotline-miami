70298364a3eecd569bb64a4abec9a562895ac9f7
Jamel <81921407+JGAMIX7@users.noreply.github.com>
2024-01-18 23:37:39 +0100

    stupid error fixed

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+const menu = document.getElementById("menu");
+const toggle = document.getElementById("toggle");
+const close = document.getElementById("close");
+
+if (toggle) {
+  toggle.addEventListener("click", () => {
+    menu.classList.add("active");
+    document.getElementById("toggle-img").src = "../images/close.svg";
+  });
+}
+
+// if (close) {
+//   close.addEventListener("click", () => {
+//     menu.classList.remove("active");
+//   });
+// }
