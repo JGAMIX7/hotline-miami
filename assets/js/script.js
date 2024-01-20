65b427c14960be6242e86a3f69794ca35cd37e84
Jamel <81921407+JGAMIX7@users.noreply.github.com>
2024-01-20 14:46:20 +0100

    ...

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,15 @@
 const menu = document.getElementById("menu");
 const toggle = document.getElementById("toggle");
 const close = document.getElementById("close");
+const img = document.getElementById("toggle-img");
 
 if (toggle) {
   toggle.addEventListener("click", () => {
     menu.classList.add("active");
-    document.getElementById("toggle-img").src = "../images/close.svg";
+    img.src = "../images/close.svg";
+  });
+} else {
+  toggle.addEventListener("click", () => {
+    menu.classList.remove("active");
   });
 }
-
-// if (toggle) {
-//   toggle.addEventListener("click", () => {
-//     menu.classList.remove("active");
-//   });
-// }
