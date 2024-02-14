13e029a15144453706150cd1af5ce4e14d9c972a
Jamel <81921407+JGAMIX7@users.noreply.github.com>
2024-02-14 21:34:54 +0100

    syntax error fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
 const menu = document.getElementById("menu");
 const toggle = document.getElementById("toggle");
 const close = document.getElementById("close");
-const img = document.getElementById("toggle-img");
-// const link = document.getElementsByClassName("nav-item");
 
 if (toggle) {
   toggle.addEventListener("click", () => {
