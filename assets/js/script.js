7cb29f9759bfe97ef9213c8776f73f1bd9fd5ac4
Jamel Ag <jamel.agricole07@gmail.com>
2024-12-27 19:19:08 +0100

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,26 @@
-const menu = document.getElementById("menu");
+const menu = document.getElementById("nav");
 const toggle = document.getElementById("toggle");
 const close = document.getElementById("close");
 
 if (toggle) {
-  toggle.addEventListener("click", () => {
-    menu.classList.add("active");
-    toggle.style.display = "none";
-  });
+   toggle.addEventListener("click", () => {
+      menu.classList.add("active");
+   });
 }
 
 if (close) {
-  close.addEventListener("click", () => {
-    menu.classList.remove("active");
-    toggle.style.display = "block";
-  });
+   close.addEventListener("click", () => {
+      menu.classList.remove("active");
+   });
 }
+
+document.addEventListener('scroll', () => {
+   const header = document.querySelector('header'); // Remplacez '.mon-element' par votre sélecteur
+   const scrollThreshold = window.innerHeight * 0.1;
+
+   if (window.scrollY > scrollThreshold) {
+      header.classList.add('active'); // Remplacez 'active' par le nom de la classe à ajouter
+   } else {
+      header.classList.remove('active'); // Remplacez 'active' par le nom de la classe à retirer
+   }
+});
