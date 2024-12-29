78077eda4867f6c6f4aae1fcbfbdee04b99e9300
Jamel Ag <81921407+JGAMIX7@users.noreply.github.com>
2024-12-29 12:59:06 +0100

    javascript updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const menu = document.getElementById("nav");
 const toggle = document.getElementById("toggle");
 const close = document.getElementById("close");
+const link = document.getElementsByClassName("nav-item");
 
 if (toggle) {
    toggle.addEventListener("click", () => {
